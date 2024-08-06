3f1109bf4501b665ac1e5dad813fb3e1cf832eb8
ChristianFMadsen <134282611+ChristianFMadsen@users.noreply.github.com>
2024-08-06 15:30:12 +0200

    Added warning for demos present in log but not directory

--- demo_renamer.py
+++ demo_renamer.py
@@ -21,7 +21,7 @@ def update_dict(dict_name, key_name, action_type, tick, ks_length=0):
             dict_name[key_name] = id + '_BM_' + tick
 
 
-demo_path = 'demo_test_dir/'
+demo_path = 'C:/Users/Christian/PycharmProjects/pythonProject/demo_test_dir'
 quoted = re.compile('"[^"]*"')
 kill_streak_pattern = re.compile('[A-Za-z]+\s[A-Za-z]+:[0-9]+')
 
@@ -33,21 +33,24 @@ for line in f:
     quoted_demo_name = quoted.findall(line)
     if len(quoted_demo_name) > 0:
         demo_name = quoted_demo_name[0].strip('"')
-        if demo_name not in demos_processed:
-            demos_processed.append(demo_name)
+        if os.path.exists(demo_path + demo_name + '.dem'):
+            if demo_name not in demos_processed:
+                demos_processed.append(demo_name)
 
-        name_and_tick_expr = line[line.find("(") + 1:line.find(")")]
-        tick = name_and_tick_expr.split()[-1]
-        if len(tick) > 3:
-            tick = tick[:len(tick) - 3] + 'k'
+            name_and_tick_expr = line[line.find("(") + 1:line.find(")")]
+            tick = name_and_tick_expr.split()[-1]
+            if len(tick) > 3:
+                tick = tick[:len(tick) - 3] + 'k'
 
-        ks = kill_streak_pattern.findall(line)
-        if len(ks) > 0:
-            ks_length = ks[0][-1]
-            update_dict(naming_dict, demo_name, 'KS', tick, ks_length)
+            ks = kill_streak_pattern.findall(line)
+            if len(ks) > 0:
+                ks_length = ks[0][-1]
+                update_dict(naming_dict, demo_name, 'KS', tick, ks_length)
 
-        if "Player bookmark" in line:
-            update_dict(naming_dict, demo_name, 'BM', tick)
+            if "Player bookmark" in line:
+                update_dict(naming_dict, demo_name, 'BM', tick)
+        else:
+            print(f'Warning: {demo_name} present in log file but not in directory')
 
 
 for old_demo_name, new_demo_name in naming_dict.items():
